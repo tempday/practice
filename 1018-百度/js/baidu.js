6fb7df4865b12800f0b882224da6a8125c6ad980
tempday <609262343@qq.com>
2017-11-01 09:22:33 +0800

    1101

--- 1018-百度/js/baidu.js
+++ 1018-百度/js/baidu.js
@@ -11,6 +11,47 @@
 	var imgBtn=document.getElementById("imgBtn");
 	var loadimg=document.getElementById("loadimg");
 	var close=document.getElementById("close");
+	var search=document.getElementById("search");
+	var keywords=document.getElementById("keywords");
+	var keyslist=document.getElementById("keyslist");
+	keyslist.onclick=function(){
+		var src=event.srcElement||event.target;
+		if(src.nodeName="LI"){
+			keywords.value=src.innerHTML;
+		}
+	}
+	keywords.onblur=function(){
+		setTimeout(function(){
+			keyslist.style.display="none";
+		}, 200);
+	}
+	keywords.onfocus=keywords.onkeyup=getWords;
+	function getWords(){
+		var key=keywords.value.replace(/^\s*|\s*$/g,"");
+		if(key){
+			//创建script标签
+			var oScript = document.createElement("script");
+			oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+key+"&cb=callback";
+			document.body.appendChild(oScript);
+			document.body.removeChild(oScript);
+		}else{
+			keyslist.innerHTML="";
+			keyslist.style.display="none";
+		}
+    }
+	function callback(data){
+		var str="";
+		if(data.s.length){
+			for(var i=0;i<data.s.length;i++){
+			str += "<li>"+data.s[i]+"</li>";
+			}
+			keyslist.innerHTML=str;
+			keyslist.style.display="block";
+		}else{
+			keyslist.style.display="none";
+		}
+	}
+	window.callback=callback;//global
 	imgBtn.onclick=function () {
 		loadimg.style.display="block";
 		return true;
@@ -18,5 +59,9 @@
 	close.onclick=function () {
 		loadimg.style.display="none";
 		return true;
+	}
+	search.onclick=function () {
+		var key=keywords.value.replace(/^\s*|\s*$/g,"");
+		key&&(location.href="http://www.baidu.com/s?wd="+key);
 	}
 }();
